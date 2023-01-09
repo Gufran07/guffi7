ef1e3872b6326f8537e2488a0b9549725f667629
Gufran07 <120317963+Gufran07@users.noreply.github.com>
2023-01-10 03:11:23 +0530

    Update script.groovy

--- Jenkinsfile-syntax/script.groovy
+++ Jenkinsfile-syntax/script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t guffi/guffi-docker:jma-2.1 .'
+        sh 'docker build -t guffi/guffi-docker:jma-2.5 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push guffi/guffi-docker:jma-2.1'
+        sh 'docker push guffi/guffi-docker:jma-2.5'
     }
 } 
 
